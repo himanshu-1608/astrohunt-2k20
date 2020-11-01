78e2e5ea4ec75c6232b7eb26647015279b69b365
Himanshu Yadav <ydv1608@gmail.com>
2020-11-01 20:46:30 +0530

    Added basic index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- Adding Materialize CSS -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Adding meta-data for the site-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Astrohunt-2k20</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
